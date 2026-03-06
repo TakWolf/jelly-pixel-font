bac8483a788d2a31c0b19a7fa3f5685cc7851be3
TakWolf <takwolf@foxmail.com>
2026-03-06 14:06:25 +0800

    许可证移除保留字体名称

--- tools/services/font_service.py
+++ tools/services/font_service.py
@@ -82,7 +82,7 @@ class DesignContext:
         builder.meta_info.manufacturer = 'TakWolf'
         builder.meta_info.designer = 'TakWolf'
         builder.meta_info.description = 'Open source Pan-CJK pixel font'
-        builder.meta_info.copyright_info = 'Copyright (c) 2026, TakWolf (https://takwolf.com), with Reserved Font Name "Jelly Pixel"'
+        builder.meta_info.copyright_info = 'Copyright (c) 2026, TakWolf (https://takwolf.com)'
         builder.meta_info.license_info = 'This Font Software is licensed under the SIL Open Font License, Version 1.1'
         builder.meta_info.vendor_url = 'https://jelly-pixel-font.takwolf.com'
         builder.meta_info.designer_url = 'https://takwolf.com'
