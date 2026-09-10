d656bff21c94e26a51b8c6acd52b44015ae2fe56
TakWolf <takwolf@foxmail.com>
2026-09-11 01:49:55 +0800

    调整字体简介

--- tools/services/font_service.py
+++ tools/services/font_service.py
@@ -79,7 +79,7 @@ class DesignContext:
         builder.meta_info.width_style = WidthStyle(width_mode.capitalize())
         builder.meta_info.manufacturer = 'TakWolf'
         builder.meta_info.designer = 'TakWolf'
-        builder.meta_info.description = 'Open source Pan-CJK pixel font'
+        builder.meta_info.description = 'Open-source Pan-Latin and Pan-CJK pixel font in a rounded sans-serif style'
         builder.meta_info.copyright_info = 'Copyright (c) 2026, TakWolf (https://takwolf.com)'
         builder.meta_info.license_info = 'This Font Software is licensed under the SIL Open Font License, Version 1.1'
         builder.meta_info.vendor_url = 'https://jelly-pixel-font.takwolf.com'
