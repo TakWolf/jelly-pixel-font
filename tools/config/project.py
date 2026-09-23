2b5cf97b3113dfb7e0cf22709dc5c875662a073b
TakWolf <takwolf@foxmail.com>
2026-09-23 18:37:43 +0800

    项目级静态配置提取到 project 中

---
+++ tools/config/project.py
@@ -0,0 +1,17 @@
+
+VERSION = '2026.09.01'
+
+FAMILY_NAME_PREFIX = 'Jelly Pixel'
+FILE_NAME_PREFIX = 'jelly-pixel'
+
+DESCRIPTION = 'Open-source Pan-Latin and Pan-CJK pixel font in a rounded sans-serif style'
+
+DESIGNER = 'TakWolf'
+DESIGNER_URL = 'https://takwolf.com'
+
+MANUFACTURER = 'TakWolf'
+VENDOR_URL = 'https://jelly-pixel-font.takwolf.com'
+
+COPYRIGHT_INFO = 'Copyright (c) 2026, TakWolf (https://takwolf.com)'
+LICENSE_INFO = 'This Font Software is licensed under the SIL Open Font License, Version 1.1'
+LICENSE_URL = 'https://github.com/TakWolf/jelly-pixel-font/blob/master/LICENSE-OFL'
